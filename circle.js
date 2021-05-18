e08d6d567721099fef5134b5d52833cfb71a543c
vuongly2411 <lyhuong241@gmail.com>
2021-05-18 20:19:16 +0700

    qsas

---
+++ circle.js
@@ -0,0 +1,8 @@
+let radius = +prompt("Ban kinh cua hinh tron: ");
+const PI = 3.14
+diameter = 2 * radius;
+circumference = 2 * PI * radius;
+area = PI * radius * radius;
+document.write('Duong kinh cua hinh tron la: ' + diameter + '<br>');
+document.write('Chu vi cua hinh tron la: ' + circumference + '<br>');
+document.write('Dien tich cua hinh tron la: ' + area);
